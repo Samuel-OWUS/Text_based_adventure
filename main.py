cd02cbd8f481748e1b64b210b4641fbb003428a8
Samuel-OWUS <119402814+Samuel-OWUS@users.noreply.github.com>
2023-04-07 01:20:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Text based adventure game
+name=str(input('Enter your name:'))
+print(f"{name} you are stuck at work")
+print("You still working and suddenly you saw a ghost, Now you have two options")
+print("1.Run. 2.Jump through the window")
+user=int(input("Choose 1 or :"))
+if user == 1:
+  print("You did it")
+elif user==2:
+     print("You are not that smart")
+else:
+     print("Please Check your input")
